0a2b50811d1f1903696e615de5a0b5cc91f82e2d
Dzone <Dzone@DESKTOP-ET8D33N>
2020-06-19 14:21:11 +0530

    Add Context folder on DataLayer Configure to startup.cs

---
+++ TestWebApplication.Data/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestWebApplication.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base (options)
+        {
+
+        }
+    }
+}
